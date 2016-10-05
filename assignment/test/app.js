49dc74170c9dfe007fbbd78a3b7ec510e2b59222
rexue70 <wangyudong53138@126.com>
2016-10-05 15:32:35 -0700

    v8

--- assignment/test/app.js
+++ assignment/test/app.js
@@ -4,7 +4,7 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://yudong:yudong@ds033086.mlab.com:33086/yudong';
 
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
