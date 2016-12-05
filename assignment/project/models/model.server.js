584526336ba8411c2363472802d5205da172bdc1
rexue70 <wangyudong53138@126.com>
2016-12-05 15:39:37 -0800

    proj3

--- assignment/project/models/model.server.js
+++ assignment/project/models/model.server.js
@@ -1,7 +1,8 @@
 module.exports = function () {
     var mongoose = require("mongoose");
-    // mongoose.connect("mongodb://yudong:yudong@ds033086.mlab.com:33086/yudong");
-    mongoose.connect("mongodb://localhost/project-fall-2017");
+    mongoose.connect("mongodb://yudong:yudong@ds033086.mlab.com:33086/yudong");
+
+    // mongoose.connect("mongodb://localhost/project-fall-2017");
 
     var userModel = require("./user/user.model.server")();
 
